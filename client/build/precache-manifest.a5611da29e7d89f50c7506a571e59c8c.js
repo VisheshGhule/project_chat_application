c1bf07aa5e9bff50209d170613f8091df6288193
Vishesh <visheshghule2@gmail.com>
2025-04-14 19:38:17 +0530

    Force-added build folder for deployment

---
+++ client/build/precache-manifest.a5611da29e7d89f50c7506a571e59c8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43590fed150e63237c7bbf69e973a0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fba90fb3c304ca8c60bf",
+    "url": "/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "70a8661ef3e27cb49619",
+    "url": "/static/js/2.d6d14563.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.d6d14563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba90fb3c304ca8c60bf",
+    "url": "/static/js/main.79854b05.chunk.js"
+  },
+  {
+    "revision": "6d6a8371bd2fbefdfa02",
+    "url": "/static/js/runtime-main.9f5ed7fe.js"
+  }
+]);
